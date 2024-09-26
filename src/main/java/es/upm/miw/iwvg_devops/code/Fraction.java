4cedb4959e4dfbabc1b825770f860555492d27af
Hongxiang Wu <90578041+Aklur@users.noreply.github.com>
2024-09-26 22:35:26 +0200

    enhanced class Fraction #6

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -33,6 +33,37 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean isProper(){ return this.numerator < this.denominator; }
+
+    public boolean isImproper(){ return this.numerator > this.denominator; }
+
+    public boolean isEquivalent(Fraction fraction){
+        int numerador1 = this.numerator * fraction.denominator;
+        int numerador2 = this.denominator * fraction.numerator;
+        return numerador1 == numerador2; }
+
+    public Fraction add(Fraction fraction){
+        int denominador = this.denominator * fraction.denominator;
+        int numerador1 = this.numerator * denominador;
+        int numerador2 = fraction.numerator * denominador;
+        this.setNumerator(numerador1 + numerador2);
+        this.setDenominator(denominador);
+        return this; }
+
+    public Fraction multiply (Fraction fraction){
+        int denominador = this.denominator * fraction.denominator;
+        int numerador = this.numerator * fraction.numerator;
+        this.setNumerator(numerador);
+        this.setDenominator(denominador);
+        return this; }
+
+    public Fraction divide (Fraction fraction){
+        int denominador = this.denominator * fraction.numerator;
+        int numerador = this.numerator * fraction.denominator;
+        this.setNumerator(numerador);
+        this.setDenominator(denominador);
+        return this; }
+
     @Override
     public String toString() {
         return "Fraction{" +
