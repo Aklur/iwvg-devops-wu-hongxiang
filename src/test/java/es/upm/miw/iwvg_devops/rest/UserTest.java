96d7b8420cf928651c94f3a467f6dfa6364a4b54
Hongxiang Wu <90578041+Aklur@users.noreply.github.com>
2024-09-26 16:43:20 +0200

    Added Test UserTest #4

---
+++ src/test/java/es/upm/miw/iwvg_devops/rest/UserTest.java
@@ -0,0 +1,82 @@
+package es.upm.miw.iwvg_devops.rest;
+
+import es.upm.miw.iwvg_devops.code.Fraction;
+import es.upm.miw.iwvg_devops.code.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    private User user;
+
+    @BeforeEach
+    void before() {
+        List<Fraction> fractionList = new ArrayList<>();
+        Fraction fraction1 = new Fraction(1,1);
+        Fraction fraction2 = new Fraction(2,2);
+        Fraction fraction3 = new Fraction(3,3);
+        fractionList.add(fraction1);
+        fractionList.add(fraction2);
+        fractionList.add(fraction3);
+        this.user = new User("1","David","Wu",fractionList);
+    }
+
+    @Test
+    void testUserNoList(){
+        User user = new User();
+        assertNotNull(user.getFractions());
+    }
+
+    @Test
+    void testUserWithInf(){
+        List<Fraction> fractionList = new ArrayList<>();
+        User user = new User("1","David","Wu",fractionList);
+        assertEquals("1",user.getId());
+        assertEquals("David",user.getName());
+        assertEquals("Wu",user.getFamilyName());
+        assertNotNull(user.getFractions());
+    }
+
+    @Test
+    void testGetId(){
+        assertEquals("1",user.getId());
+    }
+
+    @Test
+    void testGetName(){
+        assertEquals("David",user.getName());
+    }
+
+    @Test
+    void testGetFamilyName(){
+        assertEquals("Wu",user.getFamilyName());
+    }
+
+    @Test
+    void testGetFractions(){
+        assertEquals(3,user.getFractions().size());
+    }
+
+    @Test
+    void testSetFraction(){
+        List<Fraction> fractionListNew = new ArrayList<>();
+        Fraction fraction4 = new Fraction(2,2);
+        Fraction fraction5 = new Fraction(1,1);
+        fractionListNew.add(fraction4);
+        fractionListNew.add(fraction5);
+        user.setFractions(fractionListNew);
+        assertEquals(2,user.getFractions().size());
+    }
+
+    @Test
+    void testAddFraction(){
+        Fraction fraction6 = new Fraction(4,4);
+        user.addFraction(fraction6);
+        assertEquals(4,user.getFractions().size());
+    }
+
+}
